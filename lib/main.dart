7ebe24dab88b32602058548f8585eb1926dea48a
Mauricio Pacheco <pachperdev@gmail.com>
2024-01-28 23:00:07 -0500

    added Style and Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'config/router/app_router.dart';
+import 'config/theme/app_theme.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
     );
   }
 }
