6b624040e533fd47dc331ec228325063e27317d5
Mauricio Pacheco <pachperdev@gmail.com>
2024-01-30 07:54:40 -0500

    added Environment Variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'config/router/app_router.dart';
 import 'config/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
