8b810d0ac36306c2865b1c9bb825dd4485237732
Mauricio Pacheco <pachperdev@gmail.com>
2024-02-01 15:54:26 -0500

    MovieMappers - MovieDB to Movie Entity

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import '../../domain/entities/movie.dart';
+import '../models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
+        adult: movieDB.adult ?? false,
+        backdropPath: movieDB.backdropPath != null
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+            : 'https://eticketsolutions.com/demo/themes/e-ticket/img/movie.jpg',
+        genreIds: movieDB.genreIds?.map((e) => e.toString()).toList() ?? [],
+        id: movieDB.id ?? 0,
+        originalLanguage: movieDB.originalLanguage ?? '',
+        originalTitle: movieDB.originalTitle ?? '',
+        overview: movieDB.overview ?? '',
+        popularity: movieDB.popularity ?? 0.0,
+        posterPath: movieDB.posterPath != null
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+            : 'https://eticketsolutions.com/demo/themes/e-ticket/img/movie.jpg',
+        releaseDate: movieDB.releaseDate ?? DateTime.now(),
+        title: movieDB.title ?? '',
+        video: movieDB.video ?? false,
+        voteAverage: movieDB.voteAverage ?? 0.0,
+        voteCount: movieDB.voteCount ?? 0,
+      );
+}
