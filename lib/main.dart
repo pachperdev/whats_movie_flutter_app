f47213d4c907def3218992ecbf1e0eb72f4cdfc8
Mauricio Pacheco <pachperdev@gmail.com>
2024-02-01 21:05:37 -0500

    Create the repository instance - Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'config/router/app_router.dart';
 import 'config/theme/app_theme.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MainApp());
+
+  runApp(
+    const ProviderScope(child: MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
