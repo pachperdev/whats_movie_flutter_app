fa990f6fbd44abcc447a3905725fd2b8ee15c978
Mauricio Pacheco <pachperdev@gmail.com>
2024-03-24 18:34:42 -0500

    End the movie carousel

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +20,7 @@ class MoviesSlideshow extends StatelessWidget {
       child: Swiper(
         viewportFraction: 0.8,
         scale: 0.9,
-        autoplay: true,
+        // autoplay: true,
         itemCount: movies.length,
         itemBuilder: (context, index) => _Slide(movie: movies[index]),
       ),
@@ -34,6 +35,33 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10,
+          offset: Offset(0, 5),
+        ),
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(movie.backdropPath, fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress != null) {
+              return const DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.black12));
+            }
+            return FadeIn(child: child);
+          }),
+        ),
+      ),
+    );
   }
 }
