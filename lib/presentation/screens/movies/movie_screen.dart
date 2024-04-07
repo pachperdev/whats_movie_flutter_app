70bb5d0e5de6c7d177b1cf028a422a6dc2d88cb7
Mauricio Pacheco <pachperdev@gmail.com>
2024-04-07 12:24:10 -0500

    Mostrar los actores de la película

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -184,8 +184,75 @@ class _MovieDetails extends StatelessWidget {
             ],
           ),
         ),
-        const SizedBox(height: 500),
+
+        //Actores de la pelicula
+        _ActorsByMovie(movieId: movie.id.toString()),
+
+        const SizedBox(height: 50),
       ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+  const _ActorsByMovie({
+    required this.movieId,
+  });
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    if (actorsByMovie[movieId] == null) {
+      return const Center(
+        child: CircularProgressIndicator(strokeWidth: 2),
+      );
+    }
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        itemCount: actors.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            width: 135,
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath!,
+                    height: 180,
+                    width: 135,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  actor.name!,
+                  maxLines: 2,
+                ),
+                Text(
+                  actor.character!,
+                  maxLines: 2,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
