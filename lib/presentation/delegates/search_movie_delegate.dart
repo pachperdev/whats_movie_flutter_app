42663e99b59c13bd6f8308edb4fdfb2c6e68a132
Mauricio Pacheco <pachperdev@gmail.com>
2024-04-20 11:27:47 -0500

    Leading y Actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,25 +1,31 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+import '../../domain/entities/movie.dart';
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Buscar filmes...';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
+      if (query.isNotEmpty)
+        FadeIn(
+          child: IconButton(
+            icon: const Icon(Icons.clear_rounded),
+            onPressed: () {
+              query = '';
+            },
+          ),
+        ),
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      icon: const Icon(Icons.arrow_back),
+      icon: const Icon(Icons.arrow_back_ios_rounded),
       onPressed: () {
         close(context, null);
       },
