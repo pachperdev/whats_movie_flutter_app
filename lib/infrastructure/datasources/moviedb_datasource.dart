9e28549bfd5c0ba7cd90a67397dc74db281ef720
Mauricio Pacheco <pachperdev@gmail.com>
2024-04-20 10:30:23 -0500

    Add movie search functionality into datasource and repository

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -91,4 +91,16 @@ class MovieDbDataSource extends MoviesDataSource {
 
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response = await dio.get(
+      '/search/movie',
+      queryParameters: {
+        'query': query,
+      },
+    );
+
+    return _jsonToMovies(response.data);
+  }
 }
