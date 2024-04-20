76493364fa3a8dba43bfa4f671e26bfe5d0a17d1
Mauricio Pacheco <pachperdev@gmail.com>
2024-04-20 09:52:15 -0500

    Implement lazy loading for movie poster images

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -84,10 +85,14 @@ class _CustomSliverAppBar extends StatelessWidget {
           background: Stack(
             children: [
               SizedBox.expand(
-                child: Image.network(
-                  movie.posterPath,
-                  fit: BoxFit.cover,
-                ),
+                child: Image.network(movie.posterPath, fit: BoxFit.cover,
+                    loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const SizedBox();
+                  }
+
+                  return FadeIn(child: child);
+                }),
               ),
               const SizedBox.expand(
                 child: DecoratedBox(
@@ -226,13 +231,15 @@ class _ActorsByMovie extends ConsumerWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.network(
-                    actor.profilePath!,
-                    height: 180,
-                    width: 135,
-                    fit: BoxFit.cover,
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath!,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 5),
