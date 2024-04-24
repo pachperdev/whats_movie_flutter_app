bf71f9ec56563d782048b3b52546ff8dac696614
Mauricio Pacheco <pachperdev@gmail.com>
2024-04-23 23:01:10 -0500

    ShellRoute - GoRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -7,47 +7,29 @@ final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     ShellRoute(
-        builder: (context, state, child) {
-          return HomeScreen(childView: child);
-        },
-        routes: [
-          GoRoute(
-            path: '/',
-            builder: (context, state) => const HomeView(),
-            routes: [
-              GoRoute(
-                path: 'movie/:id',
-                name: MovieScreen.name,
-                builder: (context, state) {
-                  final movieId = state.pathParameters['id'] ?? 'no-id';
-                  return MovieScreen(movieId: movieId);
-                },
-              ),
-            ],
-          ),
-          GoRoute(
-            path: '/favorites',
-            builder: (context, state) => const FavoritesView(),
-          ),
-        ]),
-
-    // Rutas padre/hijo
-    // GoRoute(
-    //   path: '/',
-    //   name: HomeScreen.name,
-    //   builder: (context, state) => const HomeScreen(
-    //     childView: FavoritesView(),
-    //   ),
-    //   routes: [
-    //     GoRoute(
-    //       path: 'movie/:id',
-    //       name: MovieScreen.name,
-    //       builder: (context, state) {
-    //         final movieId = state.pathParameters['id'] ?? 'no-id';
-    //         return MovieScreen(movieId: movieId);
-    //       },
-    //     ),
-    //   ],
-    // ),
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const HomeView(),
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) => const FavoritesView(),
+        ),
+      ],
+    ),
   ],
 );
