071a93754374dbead16dfb7bb45297c82376cf7d
Mauricio Pacheco <pachperdev@gmail.com>
2024-04-24 21:54:03 -0500

    added ShellRoute

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -33,3 +33,38 @@ final appRouter = GoRouter(
     ),
   ],
 );
+
+// final appRouter = GoRouter(initialLocation: '/', routes: [
+//   // State-Preserving
+//   StatefulShellRoute.indexedStack(
+//       builder: (context, state, navigationShell) =>
+//           HomeScreen(childView: navigationShell),
+//       branches: [
+//         StatefulShellBranch(routes: <RouteBase>[
+//           GoRoute(
+//             path: '/',
+//             builder: (context, state) {
+//               return const HomeView();
+//             },
+//             routes: [
+//               GoRoute(
+//                   path: 'movie/:id',
+//                   name: MovieScreen.name,
+//                   builder: (context, state) {
+//                     final movieId = state.pathParameters['id'] ?? 'no-id';
+//                     return MovieScreen(movieId: movieId);
+//                   },
+//                 ),
+//             ]
+//           )
+//         ]),
+//         StatefulShellBranch(routes: <RouteBase>[
+//           GoRoute(
+//             path: '/favorites',
+//             builder: (context, state) {
+//               return const FavoritesView();
+//             },
+//           )
+//         ]),
+//       ]),
+// ]);
