146bb02dfd3246bede56fe6f463388eecc3d7576
Mauricio Pacheco <pachperdev@gmail.com>
2024-04-29 00:05:16 -0500

    Update dependencies and add Isar Flutter Libraries plugin

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 1. Copiar el .env.template y renombrarlo a .env
 2. Cambiar las variables de entorno(The Movie DB API KEY)
+3. cambios en la entidad, hay que ejecutar el comando: 
+```
+flutter pub run build_runner build
+```
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,8 +1,5 @@
-import 'dart:math';
-
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../domain/entities/movie.dart';
