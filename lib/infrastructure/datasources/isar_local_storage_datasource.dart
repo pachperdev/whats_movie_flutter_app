dd6c4bb34d0ab868f8146c5148a12f564c127208
Mauricio Pacheco <pachperdev@gmail.com>
2024-05-02 23:04:09 -0500

    mplement necessary methods

--- lib/infrastructure/datasources/isar_local_storage_datasource.dart
+++ lib/infrastructure/datasources/isar_local_storage_datasource.dart
@@ -26,20 +26,36 @@ class IsarLocalStorageDatasource implements LocalStorageDatasource {
   }
 
   @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+
+    final Movie? isFavoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    return isFavoriteMovie != null;
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+
+    final favoriteMovie =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+
+    if (favoriteMovie != null) {
+      //borrar
+      isar.writeTxn(() => isar.movies.delete(favoriteMovie.isarId!));
+      return;
+    }
+
+    //isertar
+    isar.writeTxn(() => isar.movies.put(movie));
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) async {
+    final isar = await db;
+
+    return isar.movies.where().offset(offset).limit(limit).findAll();
   }
 }
