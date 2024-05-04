42ccd53b5cc842fe2206f40b5c8df00157ca75e0
Mauricio Pacheco <pachperdev@gmail.com>
2024-05-04 18:02:36 -0500

    Favorite MoviesProvider

---
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whats_movie_flutter_app/domain/repositories/local_storage_repository.dart';
+
+import '../../../domain/entities/movie.dart';
+import '../providers.dart';
+
+final favoriteMoviesProvider =
+    StateNotifierProvider<StortageMoviesNotifier, Map<int, Movie>>((ref) {
+  final localStorageRepository = ref.watch(localStorageRepositoryProvider);
+
+  return StortageMoviesNotifier(localStorageRepository: localStorageRepository);
+});
+
+class StortageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+
+  StortageMoviesNotifier({required this.localStorageRepository}) : super({});
+
+  Future<List<Movie>> loadNextPage() async {
+    final movies = await localStorageRepository.loadMovies(offset: page * 10);
+    page++;
+
+    final tempMoviesMap = <int, Movie>{};
+    for (final movie in movies) {
+      tempMoviesMap[movie.id] = movie;
+    }
+
+    state = {...state, ...tempMoviesMap};
+
+    return movies;
+  }
+}
