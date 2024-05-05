18c66779fa5c6d83c618bb33cb085928d2188e7f
Mauricio Pacheco <pachperdev@gmail.com>
2024-05-05 18:45:56 -0500

    Implement lazy loading for movie images in FavoritesView

--- lib/infrastructure/datasources/isar_local_storage_datasource.dart
+++ lib/infrastructure/datasources/isar_local_storage_datasource.dart
@@ -4,7 +4,7 @@ import 'package:whats_movie_flutter_app/domain/entities/movie.dart';
 
 import '../../domain/datasources/local_storage_datasource.dart';
 
-class IsarLocalStorageDatasource implements LocalStorageDatasource {
+class IsarLocalStorageDatasource extends LocalStorageDatasource {
   late Future<Isar> db;
 
   IsarLocalStorageDatasource() {
@@ -12,14 +12,10 @@ class IsarLocalStorageDatasource implements LocalStorageDatasource {
   }
 
   Future<Isar> openDB() async {
-    final dir = await getApplicationDocumentsDirectory();
-
     if (Isar.instanceNames.isEmpty) {
-      return await Isar.open(
-        [MovieSchema],
-        inspector: true,
-        directory: dir.path,
-      );
+      final dir = await getApplicationDocumentsDirectory();
+      return await Isar.open([MovieSchema],
+          inspector: true, directory: dir.path);
     }
 
     return Future.value(Isar.getInstance());
@@ -43,15 +39,15 @@ class IsarLocalStorageDatasource implements LocalStorageDatasource {
         await isar.movies.filter().idEqualTo(movie.id).findFirst();
 
     if (favoriteMovie != null) {
-      isar.writeTxn(() => isar.movies.delete(favoriteMovie.isarId!));
+      isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
       return;
     }
 
-    isar.writeTxn(() => isar.movies.put(movie));
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, int offset = 0}) async {
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
     final isar = await db;
 
     return isar.movies.where().offset(offset).limit(limit).findAll();
